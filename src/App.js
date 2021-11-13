0d7e55f87e6c747b25d529004c90233e3ff902a3
sonikabaranwal <sonikabaranwal@gmail.com>
2021-11-13 18:57:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    showMessage: false
+  }
+  onButtonClickHandler = () => {
+   this.setState({showMessage: true});
+  };
+
+  render(){ 
+    return(<div className="App">
+     {this.state.showMessage && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id="click" onClick={this.onButtonClickHandler}>Enter</button>
+    </div>);
+
+  }
 }
 
 export default App;
